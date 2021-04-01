d410412bc6f47ae8a490bf97c93b0fb28afc4bcf
Callum Seabrook <yutubebroz@gmail.com>
2021-04-01 18:57:10 +0100

    Fixed tags not being loaded and sent properly

--- server/src/main/kotlin/org/kryptonmc/krypton/packet/out/play/PacketOutTags.kt
+++ server/src/main/kotlin/org/kryptonmc/krypton/packet/out/play/PacketOutTags.kt
@@ -5,7 +5,7 @@ import org.kryptonmc.krypton.api.registry.NamespacedKey
 import org.kryptonmc.krypton.extension.writeString
 import org.kryptonmc.krypton.extension.writeVarInt
 import org.kryptonmc.krypton.packet.state.PlayPacket
-import org.kryptonmc.krypton.registry.*
+import org.kryptonmc.krypton.registry.RegistryManager
 import org.kryptonmc.krypton.registry.tags.Tag
 import org.kryptonmc.krypton.registry.tags.TagManager
 
@@ -14,64 +14,34 @@ class PacketOutTags(
     private val tagManager: TagManager
 ) : PlayPacket(0x5B) {
 
+    private val blockRegistry: (NamespacedKey) -> Int? = {
+        registryManager.registries.blocks.entries[it]?.protocolId
+    }
+    private val fluidRegistry: (NamespacedKey) -> Int? = {
+        registryManager.registries.fluids.entries[it]?.protocolId
+    }
+    private val itemRegistry: (NamespacedKey) -> Int? = lambda@{
+        registryManager.registries.items.entries[it]?.protocolId
+    }
+    private val entityRegistry: (NamespacedKey) -> Int? = {
+        registryManager.registries.entityTypes.entries[it]?.protocolId
+    }
+
     override fun write(buf: ByteBuf) {
-        buf.writeVarInt(tagManager.blockTags.size) // number of block tags
-        buf.writeBlockTags() // block tags
-
-        buf.writeVarInt(tagManager.entityTypeTags.size) // number of entity tags
-        buf.writeEntityTags()
-
-        buf.writeVarInt(tagManager.fluidTags.size) // number of fluid tags
-        buf.writeFluidTags()
-
-        buf.writeVarInt(tagManager.itemTags.size)
-        buf.writeItemTags()
+        buf.writeTags(tagManager.blockTags, blockRegistry)
+        buf.writeTags(tagManager.itemTags, itemRegistry)
+        buf.writeTags(tagManager.fluidTags, fluidRegistry)
+        buf.writeTags(tagManager.entityTags, entityRegistry)
     }
 
-    private fun ByteBuf.writeBlockTags() {
-        val blockRegistry = registryManager.registries.blocks
-        for (tag in tagManager.blockTags) {
-            writeString(tag.name)
-            writeVarInt(tag.data.values.size)
+    private fun ByteBuf.writeTags(tags: Set<Tag>, registry: (NamespacedKey) -> Int?) {
+        writeVarInt(tags.size)
+        tags.forEach { tag ->
+            writeString(tag.name.toString())
 
-            writeTags(tagManager.getDeepTags(tag), blockRegistry)
+            val values = tag.values.filter { registry(it) != null }
+            writeVarInt(values.size)
+            values.forEach { writeVarInt(registry(it)!!) }
         }
     }
-
-    private fun ByteBuf.writeEntityTags() {
-        val entityTypeRegistry = registryManager.registries.entityTypes
-        for (tag in tagManager.entityTypeTags) {
-            writeString(tag.name)
-            writeVarInt(tag.data.values.size)
-
-            writeTags(tagManager.getDeepTags(tag), entityTypeRegistry)
-        }
-    }
-
-    private fun ByteBuf.writeFluidTags() {
-        val fluidRegistry = registryManager.registries.fluids
-        for (tag in tagManager.fluidTags) {
-            writeString(tag.name)
-            writeVarInt(tag.data.values.size)
-
-            writeTags(tagManager.getDeepTags(tag), fluidRegistry)
-        }
-    }
-
-    private fun ByteBuf.writeItemTags() {
-        val itemRegistry = registryManager.registries.items
-        for (tag in tagManager.itemTags) {
-            writeString(tag.name)
-            writeVarInt(tag.data.values.size)
-
-            writeTags(tagManager.getDeepTags(tag), itemRegistry)
-        }
-    }
-
-    private fun ByteBuf.writeTags(tags: List<Tag>, registry: Registry) {
-        tags.forEach { writeVarInt(registry.entries.getValue(NamespacedKey(value = it.name)).protocolId) }
-    }
-
-    private fun ByteBuf.writeTags(tags: List<Tag>, registry: DefaultedRegistry) =
-        writeTags(tags, Registry(registry.protocolId, registry.entries))
 }
--- server/src/main/kotlin/org/kryptonmc/krypton/registry/tags/Tags.kt
+++ server/src/main/kotlin/org/kryptonmc/krypton/registry/tags/Tags.kt
@@ -1,10 +1,32 @@
 package org.kryptonmc.krypton.registry.tags
 
 import kotlinx.serialization.Serializable
+import org.kryptonmc.krypton.api.registry.NamespacedKey
+import org.kryptonmc.krypton.api.registry.toNamespacedKey
 
-data class Tag(
-    val name: String,
-    val data: TagData
+data class Tag(val name: NamespacedKey, val values: MutableSet<NamespacedKey> = mutableSetOf()) {
+
+    constructor(manager: TagManager, name: NamespacedKey, type: String, previous: Tag, data: TagData) : this(name) {
+        if (!data.replace) values += previous.values
+        data.values.forEach {
+            if (it.startsWith('#')) {
+                val subTag = manager.load(it.drop(1).toNamespacedKey(), type)
+                values += subTag.values
+            } else {
+                values += it.toNamespacedKey()
+            }
+        }
+    }
+
+    companion object {
+
+        val EMPTY = Tag(NamespacedKey(value = "krypton:empty"))
+    }
+}
+
+data class RequiredTag(
+    val type: TagType,
+    val name: NamespacedKey
 )
 
 @Serializable
@@ -12,3 +34,11 @@ data class TagData(
     val replace: Boolean,
     val values: List<String>
 )
+
+enum class TagType {
+
+    BLOCKS,
+    ITEMS,
+    FLUIDS,
+    ENTITY_TYPES
+}
