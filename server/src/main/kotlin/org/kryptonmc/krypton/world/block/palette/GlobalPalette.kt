d08da3d88669a725f8587610449a6f612d1cbbba
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-05-06 15:58:25 +0100

    Relicense the server under GPL 3

--- server/src/main/kotlin/org/kryptonmc/krypton/world/block/palette/GlobalPalette.kt
+++ server/src/main/kotlin/org/kryptonmc/krypton/world/block/palette/GlobalPalette.kt
@@ -1,19 +1,19 @@
 /*
- * This file is part of the Krypton project, licensed under the GNU Lesser General Public License v3.0
+ * This file is part of the Krypton project, licensed under the GNU General Public License v3.0
  *
  * Copyright (C) 2021 KryptonMC and the contributors of the Krypton project
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package org.kryptonmc.krypton.world.block.palette
