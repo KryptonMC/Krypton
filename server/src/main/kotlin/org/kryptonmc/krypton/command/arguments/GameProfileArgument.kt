7ef09a22f17961912d5744aaf51c46a63f77be12
TheRealJan <jan.m.tennert@gmail.com>
2021-08-01 19:15:52 +0200

    Move GameProfileArgument.kt out of a separate package

--- server/src/main/kotlin/org/kryptonmc/krypton/command/arguments/GameProfileArgument.kt
+++ server/src/main/kotlin/org/kryptonmc/krypton/command/arguments/GameProfileArgument.kt
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package org.kryptonmc.krypton.command.arguments.gameprofile
+package org.kryptonmc.krypton.command.arguments
 
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
