d5caf3e11a72593e68dd39a6ba6d509c2cb5dd95
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-08-01 20:04:55 +0100

    Added the remainder of the biome traits

---
+++ server/src/main/kotlin/org/kryptonmc/krypton/world/biome/BiomeConstants.kt
@@ -0,0 +1,94 @@
+/*
+ * This file is part of the Krypton project, licensed under the GNU General Public License v3.0
+ *
+ * Copyright (C) 2021 KryptonMC and the contributors of the Krypton project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.kryptonmc.krypton.world.biome
+
+interface BiomeConstants {
+
+    companion object {
+
+        const val OCEAN = 0
+        const val PLAINS = 1
+        const val DESERT = 2
+        const val MOUNTAINS = 3
+        const val FOREST = 4
+        const val TAIGA = 5
+        const val SWAMP = 6
+        const val RIVER = 7
+        const val FROZEN_OCEAN = 10
+        const val FROZEN_RIVER = 11
+        const val SNOWY_TUNDRA = 12
+        const val SNOWY_MOUNTAINS = 13
+        const val MUSHROOM_FIELDS = 14
+        const val MUSHROOM_FIELD_SHORE = 15
+        const val BEACH = 16
+        const val DESERT_HILLS = 17
+        const val WOODED_HILLS = 18
+        const val TAIGA_HILLS = 19
+        const val MOUNTAIN_EDGE = 20
+        const val JUNGLE = 21
+        const val JUNGLE_HILLS = 22
+        const val JUNGLE_EDGE = 23
+        const val DEEP_OCEAN = 24
+        const val STONE_SHORE = 25
+        const val SNOWY_BEACH = 26
+        const val BIRCH_FOREST = 27
+        const val BIRCH_FOREST_HILLS = 28
+        const val DARK_FOREST = 29
+        const val SNOWY_TAIGA = 30
+        const val SNOWY_TAIGA_HILLS = 31
+        const val GIANT_TREE_TAIGA = 32
+        const val GIANT_TREE_TAIGA_HILLS = 33
+        const val WOODED_MOUNTAINS = 34
+        const val SAVANNA = 35
+        const val SAVANNA_PLATEAU = 36
+        const val BADLANDS = 37
+        const val WOODED_BADLANDS_PLATEAU = 38
+        const val BADLANDS_PLATEAU = 39
+        const val WARM_OCEAN = 44
+        const val LUKEWARM_OCEAN = 45
+        const val COLD_OCEAN = 46
+        const val DEEP_WARM_OCEAN = 47
+        const val DEEP_LUKEWARM_OCEAN = 48
+        const val DEEP_COLD_OCEAN = 49
+        const val DEEP_FROZEN_OCEAN = 50
+        const val SUNFLOWER_PLAINS = 129
+        const val DESERT_LAKES = 130
+        const val GRAVELLY_MOUNTAINS = 131
+        const val FLOWER_FOREST = 132
+        const val TAIGA_MOUNTAINS = 133
+        const val SWAMP_HILLS = 134
+        const val ICE_SPIKES = 140
+        const val MODIFIED_JUNGLE = 149
+        const val MODIFIED_JUNGLE_EDGE = 151
+        const val TALL_BIRCH_FOREST = 155
+        const val TALL_BIRCH_HILLS = 156
+        const val DARK_FOREST_HILLS = 157
+        const val SNOWY_TAIGA_MOUNTAINS = 158
+        const val GIANT_SPRUCE_TAIGA = 160
+        const val GIANT_SPRUCE_TAIGA_HILLS = 161
+        const val MODIFIED_GRAVELLY_MOUNTAINS = 162
+        const val SHATTERED_SAVANNA = 163
+        const val SHATTERED_SAVANNA_PLATEAU = 164
+        const val ERODED_BADLANDS = 165
+        const val MODIFIED_WOODED_BADLANDS_PLATEAU = 166
+        const val MODIFIED_BADLANDS_PLATEAU = 167
+        const val BAMBOO_JUNGLE = 168
+        const val BAMBOO_JUNGLE_HILLS = 169
+    }
+}
---
+++ server/src/main/kotlin/org/kryptonmc/krypton/world/biome/layer/traits/AreaTransformer2.kt
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the Krypton project, licensed under the GNU General Public License v3.0
+ *
+ * Copyright (C) 2021 KryptonMC and the contributors of the Krypton project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.kryptonmc.krypton.world.biome.layer.traits
+
+import org.kryptonmc.krypton.world.biome.area.Area
+import org.kryptonmc.krypton.world.biome.area.AreaFactory
+import org.kryptonmc.krypton.world.biome.context.BigContext
+import org.kryptonmc.krypton.world.biome.context.Context
+
+interface AreaTransformer2 : DimensionTransformer {
+
+    operator fun invoke(context: Context, firstParent: Area, secondParent: Area, x: Int, z: Int): Int
+
+    fun <A : Area> run(context: BigContext<A>, firstParentFactory: AreaFactory<A>, secondParentFactory: AreaFactory<A>) = AreaFactory {
+        val firstParent = firstParentFactory.create()
+        val secondParent = secondParentFactory.create()
+        context.createArea({ x, z ->
+            context.initRandom(x.toLong(), z.toLong())
+            invoke(context, firstParent, secondParent, x, z)
+        }, firstParent, secondParent)
+    }
+}
