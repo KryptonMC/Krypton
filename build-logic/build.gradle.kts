477b031deaf768149f2db5c07a3ddde24350defa
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-12-13 17:12:26 +0000

    Updated detekt

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation("gradle.plugin.org.cadixdev.gradle", "licenser", "0.6.1")
     implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.6.0")
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.4.30")
-//    implementation("org.spongepowered", "vanillagradle", "0.2.1-SNAPSHOT")
-    implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.19.0-RC1")
+    implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.19.0")
     implementation("gradle.plugin.com.github.johnrengelman", "shadow", "7.1.0")
 }
