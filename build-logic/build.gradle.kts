4c8a822453d0e4959d1c61187b85f885ae12f106
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-12-20 21:08:45 +0000

    Updated Gradle, plugins, and dependencies

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 
 dependencies {
     implementation("gradle.plugin.org.cadixdev.gradle", "licenser", "0.6.1")
-    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.6.0")
+    implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.6.10")
     implementation("org.jetbrains.dokka", "dokka-gradle-plugin", "1.6.0")
     implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.19.0")
-    implementation("gradle.plugin.com.github.johnrengelman", "shadow", "7.1.0")
+    implementation("gradle.plugin.com.github.johnrengelman", "shadow", "7.1.1")
 }
