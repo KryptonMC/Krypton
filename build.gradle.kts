dce28bbbd747e6b6602fe5059df49444f8353685
Callum Seabrook <callum.seabrook@prevarinite.com>
2021-12-20 22:36:42 +0000

    A version bump might be nice for big API changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "org.kryptonmc"
-    version = "0.59.2"
+    version = "0.60"
     description = "Free and open-source Minecraft server software, written from the ground up."
 }
 
