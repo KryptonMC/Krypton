0ccdf8f7fa4ced30aa617b347ddebb230ba668e3
BomBardyGamer <callum.seabrook@prevarinite.com>
2023-02-04 13:36:34 +0000

    Deploying to docs from @ KryptonMC/Krypton@6a98d9dec73364f4934df413843dc59dbf80ff47 🚀

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":server:dokkaHtmlPartial/main":[],":generators:dokkaHtmlPartial/main":[],":api:dokkaHtmlPartial/main":[],":annotation-processor:dokkaHtmlPartial/main":[]}'
+sourceset_dependencies = '{":server:dokkaHtmlPartial/main":[],":bans-plugin:dokkaHtmlPartial/main":[],":internal-ap:dokkaHtmlPartial/main":[],":whitelist-plugin:dokkaHtmlPartial/main":[],":generators:dokkaHtmlPartial/main":[],":api:dokkaHtmlPartial/main":[],":internal-annotations:dokkaHtmlPartial/main":[],":annotation-processor:dokkaHtmlPartial/main":[]}'
