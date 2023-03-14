779664714575615c6cc5cb233de6c1ea967b8d09
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-03-14 13:56:07 +0000

    Relicense the project under Apache 2

--- api/src/main/kotlin/org/kryptonmc/api/plugin/PluginContainer.kt
+++ api/src/main/kotlin/org/kryptonmc/api/plugin/PluginContainer.kt
@@ -1,11 +1,20 @@
 /*
- * This file is part of the Krypton API, and originates from the Velocity API,
- * licensed under the MIT license.
+ * This file is part of the Krypton project, and originates from the Velocity project,
+ * licensed under the Apache License v2.0
  *
  * Copyright (C) 2018 Velocity Contributors
  *
- * This project is licensed under the terms of the MIT license.
- * For more details, please reference the LICENSE file in the api top-level directory.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * For the original file that this file is derived from, see here:
  * https://github.com/VelocityPowered/Velocity/blob/dev/1.1.0/api/src/main/java/com/velocitypowered/api/plugin/PluginContainer.java
--- server/src/main/kotlin/org/kryptonmc/krypton/console/BrigadierHighlighter.kt
+++ server/src/main/kotlin/org/kryptonmc/krypton/console/BrigadierHighlighter.kt
@@ -1,23 +1,22 @@
 /*
  * This file is part of the Krypton project, and originates from the Sponge project,
- * licensed under the terms of the GNU General Public License v3.0
+ * licensed under the terms of the Apache License v2.0
  *
  * Copyright (c) SpongePowered <https://www.spongepowered.org>
  * Copyright (c) contributors
- * Copyright (C) 2021-2022 KryptonMC and the contributors of the Krypton project
+ * Copyright (C) 2021-2023 KryptonMC and the contributors of the Krypton project
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * For the original file that this file is derived from, see here:
  * https://github.com/SpongePowered/Sponge/blob/eacd4f1c956cde1c87f2190145ef968ea05f0318/vanilla/src/main/java/org/spongepowered/vanilla/chat/console/BrigadierHighlighter.java
